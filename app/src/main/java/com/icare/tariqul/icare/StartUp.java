a88cae59c22a64b0f067dc400bb0fb79ba4bdbb7
Mobile App Develop <pkroy121@gmail.com>
2015-06-15 13:08:35 +0600

    "add method"

--- app/src/main/java/com/icare/tariqul/icare/StartUp.java
+++ app/src/main/java/com/icare/tariqul/icare/StartUp.java
@@ -1,9 +1,24 @@
 package com.icare.tariqul.icare;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Mobile App Develop on 15-6-15.
  */
-public class StartUp {
+public class StartUp extends Activity  {
+
+int a;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
 
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
